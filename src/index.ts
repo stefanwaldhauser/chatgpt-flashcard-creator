6ac840d95c4ed8d6a0423e75b45c7c9707b078af
Stefan Waldhauser <stefan.waldhauser@proton.me>
2023-09-03 21:12:11 +0200

    Bump version to 0.3.1

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { outputFlashCards } from "./output";
 async function run() {
   const program = new Command()
     .name("chatgpt-flashcard-creator")
-    .version("0.3.0")
+    .version("0.3.1")
     .requiredOption("-k, --key <chatGPTApiKey>", "API Key of ChatGPT")
     .requiredOption(
       "-w, --wordLang <language>",
